22cd42546176748d58bcfcc5dc8d55e86a6d8544
marco <marcothemars@gmail.com>
2021-12-07 21:01:58 +0200

    Init

---
+++ main.py
@@ -0,0 +1,48 @@
+"""
+module documentation here
+"""
+
+
+def read_write_relation(path):
+    """
+    Documentation here
+    """
+    pass
+
+
+def r_s_closure_search():
+    """
+    ***Search for reflexive / symetrical closure of binary relation***
+    Documentation here
+    """
+    pass
+
+
+def t_closure_search():
+    """
+    ***transitive closure search***
+    Documentation here
+    """
+    pass
+
+
+def check_t_closure():
+    """
+    Documentation here
+    """
+    pass
+
+
+def create_equivalence_classes():
+    """
+    Documentation here
+    """
+    pass
+
+
+def count_transitive_closures():
+    """
+    Documentation here
+    """
+    pass
+
