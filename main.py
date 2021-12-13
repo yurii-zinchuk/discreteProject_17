7c9cbad455265e89ae488d834487f5fa3eb8363a
Yurii Zinchuk <yurii.zinchuk@ucu.edu.ua>
2021-12-13 18:06:14 +0200

    finished 3 func, wrote dfs and matrix creation funcs

--- main.py
+++ main.py
@@ -1,40 +1,43 @@
 """
-module documentation here
+This module contains functions to work with graphs.
+Functions are able to analise graphs' connected components,
+strongly connected components, briges and connection points.
 """
+
 
 import csv
 
 
-def read_graph(path: str) -> set:
+def read_graph(path: str) -> list:
     """Return a graph, read from file,
-    as a set of tuples. Each tuple
+    as a list of tuples. Each tuple
     represents an edge.
 
     Args:
         path (str): Path to csv file with graph
 
     Returns:
-        set: Graph, read from file
+        list: Graph, read from file
     """
 
-    graph = set()
+    graph = list()
 
     with open(path, 'r') as file:
         reader = csv.reader(file, delimiter=' ')
         for line in reader:
-            graph.add((int(line[0]), int(line[1])))
+            graph.append((int(line[0]), int(line[1])))
 
     return graph
 
 
-def write_graph(path: str, graph: set) -> None:
+def write_graph(path: str, graph: list) -> None:
     """Writes graph represented as
-    set of tuples, where tuple is an edge,
+    list of tuples, where tuple is an edge,
     in a csv file.
 
     Args:
         path (str): Path to csv file
-        graph (set): Graph, represented by set
+        graph (list): Graph, represented by list
     """
 
     with open(path, 'w') as file:
@@ -43,11 +46,81 @@ def write_graph(path: str, graph: set) -> None:
             writer.writerow(edge)
 
 
-def connected_components():
+def create_adj_matrix(graph: list) -> dict:
+    """Return adjacency matrix of a graph,
+    given the list of it's edges.
+
+    Args:
+        graph (set): Graph as a list of edges
+
+    Returns:
+        dict: Adjacency matrix
     """
-    Documentation here
+
+    adj_matrix = dict()
+
+    for node1, node2 in graph:
+        if node1 not in adj_matrix:
+            adj_matrix[node1] = {node2}
+        else:
+            adj_matrix[node1].add(node2)
+
+        if node2 not in adj_matrix:
+            adj_matrix[node2] = {node1}
+        else:
+            adj_matrix[node2].add(node1)
+
+    return adj_matrix
+
+
+def dfs(graph: dict, start: int) -> set:
+    """Return a sequence of nodes after dfs.
+
+    Args:
+        graph (dict): Adjacency matrix of a graph
+        start (int): Starting node
+
+    Returns:
+        set: Nodes visited after dfs
     """
-    pass
+
+    stack, visited = [start], set()
+
+    while stack:
+        node = stack.pop()
+        if node in visited:
+            continue
+
+        for neighbour in graph[node]:
+            stack.append(neighbour)
+
+        visited.add(node)
+
+    return visited
+
+
+def connected_components(graph: list) -> list:
+    """Return a list containing conncected components.
+    Connected component is identified by the node with least number
+    that belongs to a the component.
+
+    Args:
+        graph (list): Graph as a list of edges
+
+    Returns:
+        list: Connected components
+    """
+
+    matrix_graph = create_adj_matrix(graph[1:])
+    nodesleft = set(graph.keys())
+    components = []
+
+    while nodesleft:
+        con_component = dfs(matrix_graph, nodesleft.pop())
+        nodesleft.difference_update(con_component)
+        components.append(min(con_component))
+
+    return components
 
 
 def strongly_connected_components():
@@ -72,5 +145,4 @@ def bridges():
 
 
 if __name__ == "__main__":
-    # write_graph('new.csv', read_graph('graphs/graphs/graph_100_1942_0.csv'))
     pass
